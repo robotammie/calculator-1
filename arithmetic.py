b659db1ee69f09840206fcf95c7738227d65c87f
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:17:51 -0700

    Added Doc Strings

--- arithmetic.py
+++ arithmetic.py
@@ -1,9 +1,16 @@
 def add(num1, num2):
+    """Adds num1 and num2"""
+
     result = num1 + num2
+
     return result
 
+
 def subtract(num1, num2):
+    """Subtracts num2 from num1"""
+
     return num1 - num2
+
 
 def multiply(num1, num2):
     pass
